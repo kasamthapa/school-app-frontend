24dfc5b2bf8d6542daf573c327c1791e6a9024c4
Kasam Thapa Magar <kasamthapa89@gmail.com>
2025-11-21 23:27:23 +0545

    dd

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,12 +1,12 @@
 // src/lib/api.ts
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.VITE_API_URL
-//   ?
-//   : "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:5000/api";
 
 const api = axios.create({
-  baseURL: "https://school-app-backend-9kjf.onrender.com/api",
+  baseURL: BASE_URL,
   withCredentials: true,
   headers: {
     Accept: "application/json",
